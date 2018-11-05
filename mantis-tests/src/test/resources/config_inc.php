da6b6331743c3414922801e094a30113959eae0f
Yuriy <Seaman_on_watch@mail.ru>
2018-11-05 17:55:29 +0300

    Передача файлов на удалённую машину по протоколу FTP

---
+++ mantis-tests/src/test/resources/config_inc.php
@@ -0,0 +1,8 @@
+<?php
+$g_hostname = 'localhost';
+$g_db_type = 'mysql';
+$g_database_name = 'bugtracker1';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_hostname = 'localhost';
+$g_signup_use_captcha = OFF;
