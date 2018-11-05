328fd700d0f1a31428c8e04f0dd1c18642712640
Yuriy <Seaman_on_watch@mail.ru>
2018-11-05 12:40:07 +0300

    Fix.Прямое взаимодействие с сервером по протоколу HTTP (mantisbt-1.2.19)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/HttpSession.java
@@ -24,7 +24,7 @@ public class HttpSession {
     httpclient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build();
   }
   public boolean login (String username, String password) throws Exception{
-    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login_page.php");
+    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
     List<NameValuePair> params = new ArrayList<NameValuePair>();
     params.add(new BasicNameValuePair("username", username));
     params.add(new BasicNameValuePair("password", password));
