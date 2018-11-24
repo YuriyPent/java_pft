9c97fa2912c7f4716afffce4b87244a2f2a33ba7
Yuriy <Seaman_on_watch@mail.ru>
2018-11-24 20:43:13 +0300

    s2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -24,7 +24,7 @@ public class TestBase {
   protected static final ApplicationManager app
           = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
-  @BeforeSuite //(alwaysRun = true)
+  @BeforeSuite (alwaysRun = true)
   public void setUp() throws Exception {
     app.init();
   }
